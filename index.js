262cfa09e6a817f11ed3c1286271809411922b12
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-07-15 23:55:05 +0200

    refactor(index): update fakerModules

--- index.js
+++ index.js
@@ -25,23 +25,33 @@ module.exports.fields = function(which){
     return Object.keys(faker[which]);
 };
 
+/**
+ * @type {(keyof import('@faker-js/faker').Faker)[]}
+ */
 module.exports.fakerMethods = [
-    'names',
     'address',
-    'phone',
-    'internet',
-    'company',
-    'image',
-    'lorem',
-    'helpers',
-    'date',
-    'random',
-    'finance',
-    'hacker',
-    'locales',
-    'definitions',
-    'local',
-    'system',
+    'animal',
+    'color',
     'commerce',
-    'database'
+    'company',
+    'database',
+    'date',
+    'definitions',
+    'fake',
+    'finance',
+    'git',
+    'hacker',
+    'helpers',
+    'image',
+    'internet',
+    'locale',
+    'locales',
+    'lorem',
+    'name',
+    'phone',
+    'random',
+    'science',
+    'system',
+    'vehicle',
+    'word',
 ];

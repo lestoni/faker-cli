fbf2d6ca785e7e8cf851873a1900cdefe31cfb17
xDivisionByZerox <leyla.jaehnig@gmx.de>
2023-01-11 00:12:44 +0100

    test: fix failing tests

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,40 +1,34 @@
 module.exports = [{
-    fakerMethod: 'address',
-    output: ['country', 'city', 'province']
+  fakerMethod: 'address',
+  output: ['country', 'city', 'province']
 },{
-    fakerMethod: 'company',
-    output: ['bsNoun', 'bsBuzz', 'startups']
+  fakerMethod: 'company',
+  output: ['bsNoun', 'bsBuzz', 'startups']
 },{
-    fakerMethod: 'date',
-    output: ['past', 'future', 'calendar']
+  fakerMethod: 'date',
+  output: ['past', 'future', 'calendar']
 },{
-    fakerMethod: 'finance',
-    output: ['accountName', 'mask', 'investors']
+  fakerMethod: 'finance',
+  output: ['accountName', 'mask', 'investors']
 },{
-    fakerMethod: 'internet',
-    output: ['avatar', 'userName', 'protocols']
+  fakerMethod: 'internet',
+  output: ['avatar', 'userName', 'protocols']
 },{
-    fakerMethod: 'name',
-    output: ['firstName', 'prefix', 'surname']
+  fakerMethod: 'name',
+  output: ['firstName', 'prefix', 'surname']
 },{
-    fakerMethod: 'phone',
-    output: ['phoneNumber', 'phoneNumberFormat', 'emergency']
+  fakerMethod: 'phone',
+  output: ['phoneNumber', 'phoneNumberFormat', 'emergency']
 },{
-    fakerMethod: 'random',
-    output: ['uuid', 'word', 'checks']
+  fakerMethod: 'random',
+  output: ['alpha', 'word', 'checks']
 },{
-    fakerMethod: 'hacker',
-    output: ['noun', 'phrase', 'exploits']
+  fakerMethod: 'hacker',
+  output: ['noun', 'phrase', 'exploits']
 },{
-    fakerMethod: 'helpers',
-    output: ['userCard', 'createTransaction', 'halp']
+  fakerMethod: 'image',
+  output: ['avatar', 'sports', 'filter']
 },{
-    fakerMethod: 'helpers',
-    output: ['userCard', 'createTransaction', 'halp']
-},{
-    fakerMethod: 'image',
-    output: ['avatar', 'sports', 'filter']
-},{
-    fakerMethod: 'lorem',
-    output: ['words', 'sentence', 'ipsum']
+  fakerMethod: 'lorem',
+  output: ['words', 'sentence', 'ipsum']
 }];
